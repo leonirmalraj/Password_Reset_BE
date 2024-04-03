22ce1924f7c449baaef173132cc661abe6e999d0
leonirmalraj <leo@cloudrevelinnovation.com>
2024-04-03 09:45:16 +0530

    front end side change

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -115,7 +115,7 @@ const forgotPassword = async (req, res) => {
         <p>Dear ${user.firstName} ${user.lastName},</p>
         <p>We received a request to reset your password. Here is your One-Time Password (OTP): <strong>${OTP}</strong></p>
         <p>Please click the following link to reset your password:</p>
-        <a href="http://localhost:5173/reset-password">Reset Password</a>
+        <a href="https://golden-peony-c07453.netlify.app/reset-password">Reset Password</a>
         <p>If you did not make this request, please ignore this email.</p>
         <p>Thank you</p>
         <p>From Validation</p>
